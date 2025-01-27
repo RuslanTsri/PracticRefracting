21ce7394c4496675da39722b358fa08081c3e2e0
Руслан <ipz234_tsri@student.ztu.edu.ua>
2025-01-27 21:36:10 +0200

    Revert "Add improvements to `script1.js`: optimized"

--- JS/script1.js
+++ JS/script1.js
@@ -1,11 +1,11 @@
-// Функція для отримання довжини кожного абзацу
-function getParagraphLengths() {
-    let paragraphs = document.querySelectorAll('p');
-    return Array.from(paragraphs, paragraph => paragraph.innerText.length);
-}
+// Отримуємо всі абзаци та обчислюємо їх довжину
+let paragraphs = document.querySelectorAll('p');
+let pArray = [];
 
-// Викликаємо функцію для отримання довжин
-let pArray = getParagraphLengths();
+paragraphs.forEach(paragraph => {
+    pArray.push(paragraph.innerText.length);  // Додаємо довжину кожного абзацу в масив
+});
+
 console.log(pArray);  // Виводимо масив з довжинами абзаців
 
 // Масив чисел для подальших операцій
@@ -48,3 +48,4 @@ console.log("Сортований масив: ", arr.sort((a, b) => a - b));
 
 // Реверс масиву після сортування
 console.log("Реверсований масив: ", arr.reverse());
+

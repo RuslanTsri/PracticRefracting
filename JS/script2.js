94bb265961825541867a58338b0584e5406d2595
ipz234_tsri <ipz234_tsri@student.ztu.edu.ua>
2025-01-27 16:01:25 +0200

    Fixed issue #3

--- JS/script2.js
+++ JS/script2.js
@@ -11,7 +11,8 @@ divTags.forEach(div => {
         // Проходимо через всі td, починаючи з другого, з кроком 2
         for (let i = 1; i < tdElements.length; i += 2) {
             // Додаємо клас "selected" до кожного другого td
-            tdElements[i].className = "selected";
+            tdElements[i].classList.add("selected");
+
         }
     });
 });
